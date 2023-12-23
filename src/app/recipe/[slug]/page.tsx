c608c4e8093b4c9e95b8e2d003e31e585027d231
Matt Alton <mattalton97@gmail.com>
2023-12-23 11:25:10 +0000

    Added back button on recipe page

--- src/app/recipe/[slug]/page.tsx
+++ src/app/recipe/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { RecipeStats } from "@/types/Recipe";
 import { cookies } from "next/headers";
 import { getRecipe } from "@/services/api";
 import { Suspense } from "react";
+import Link from "next/link";
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const slug = params.slug;
@@ -31,6 +32,13 @@ export default async function Page({ params }: { params: { slug: string } }) {
 
   return (
     <div className="flex flex-col gap-5">
+      <Link
+        href="/"
+        className="transition-colors rounded bg-gray-600 hover:bg-gray-700 py-2 px-4 me-auto"
+      >
+        Back
+      </Link>
+
       <RecipeStatsCard recipeStats={recipeStats} showLink={false} />
 
       <Suspense fallback={<div>Loading...</div>}>
