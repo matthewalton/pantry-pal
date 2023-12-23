5481e549e5136259060aa61b024a8d811faa1537
Matt Alton <mattalton97@gmail.com>
2023-12-23 11:19:04 +0000

    Added loading state when fetching recipe

--- src/app/recipe/[slug]/page.tsx
+++ src/app/recipe/[slug]/page.tsx
@@ -2,6 +2,7 @@ import RecipeStatsCard from "@/components/Recipe/RecipeStatsCard";
 import { RecipeStats } from "@/types/Recipe";
 import { cookies } from "next/headers";
 import { getRecipe } from "@/services/api";
+import { Suspense } from "react";
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const slug = params.slug;
@@ -20,17 +21,21 @@ export default async function Page({ params }: { params: { slug: string } }) {
     throw new Error(`Could not find recipe with slug: ${slug}`);
   }
 
-  const recipe = await getRecipe(
+  const recipeData = getRecipe(
     slug,
     recipeStats.difficulty,
     recipeStats.portions
   );
 
+  const recipe = await Promise.resolve(recipeData);
+
   return (
     <div className="flex flex-col gap-5">
       <RecipeStatsCard recipeStats={recipeStats} showLink={false} />
 
-      <div>{recipe}</div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div>{recipe}</div>
+      </Suspense>
     </div>
   );
 }
