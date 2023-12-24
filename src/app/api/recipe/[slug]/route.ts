af4fb017b97fb0f61354de3b956e03484d8c0494
Matt Alton <mattalton97@gmail.com>
2023-12-24 20:55:16 +0000

    Fixed npm build error

--- src/app/api/recipe/[slug]/route.ts
+++ src/app/api/recipe/[slug]/route.ts
@@ -19,41 +19,33 @@ export async function GET(
   const difficulty = searchParams.get("difficulty");
   const portions = searchParams.get("portions");
 
-  let messages = [
-    {
-      role: "system",
-      content: "You are a helpful assistant that provides a recipe.",
-    },
-    { role: "user", content: `Create a recipe for ${slug}.` },
-    {
-      role: "assistant",
-      content: "The recipe should have the following details:",
-    },
-  ];
-
-  if (difficulty) {
-    messages.push({
-      role: "assistant",
-      content: `Difficulty: ${difficulty}/10`,
-    });
-  }
-
-  if (portions) {
-    messages.push({
-      role: "assistant",
-      content: `Portions: ${portions}`,
-    });
-  }
-
-  messages.push({
-    role: "user",
-    content: "Provide the list of ingredients and steps to make the recipe.",
-  });
-
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     temperature: 0.7,
-    messages: messages,
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant that provides a recipe.",
+      },
+      { role: "user", content: `Create a recipe for ${slug}.` },
+      {
+        role: "assistant",
+        content: "The recipe should have the following details:",
+      },
+      {
+        role: "assistant",
+        content: `Difficulty: ${difficulty}/10`,
+      },
+      {
+        role: "assistant",
+        content: `Portions: ${portions}`,
+      },
+      {
+        role: "user",
+        content:
+          "Provide the list of ingredients and steps to make the recipe.",
+      },
+    ],
   });
 
   return NextResponse.json(response.choices[0].message.content);
