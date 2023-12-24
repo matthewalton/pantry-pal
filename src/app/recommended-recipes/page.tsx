18ab9cf612c87a67f8a5aed05d3646655294472c
Matt Alton <mattalton97@gmail.com>
2023-12-24 20:53:29 +0000

    Added back button to recommended recipes page

--- src/app/recommended-recipes/page.tsx
+++ src/app/recommended-recipes/page.tsx
@@ -2,6 +2,7 @@ import { cookies } from "next/headers";
 import { fetchRecipes } from "@/services/api";
 import { RecipeStats } from "@/types/Recipe";
 import RecipeStatsCard from "@/components/Recipe/RecipeStatsCard";
+import Link from "next/link";
 
 export default async function Page() {
   const foodList = JSON.parse(
@@ -18,6 +19,13 @@ export default async function Page() {
 
   return (
     <div className="flex flex-col gap-5">
+      <Link
+        href="/"
+        className="transition-colors rounded bg-gray-600 hover:bg-gray-700 py-2 px-4 me-auto"
+      >
+        Back
+      </Link>
+
       {recipes?.map((recipe, index) => (
         <RecipeStatsCard key={index} recipeStats={recipe} showLink={true} />
       ))}
