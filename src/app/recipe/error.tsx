70a2470b6658e17bd6fe81e08924b15bd6d3b035
Matt Alton <mattalton97@gmail.com>
2023-12-25 20:24:35 +0000

    Added error page to single recipe page

---
+++ src/app/recipe/error.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Link from "next/link";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex flex-col gap-2.5">
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+      <Link href="/">Go Back</Link>
+    </div>
+  );
+}
