4f3b88dccb10ec7e82477706f28987f88d5f53fe
Matt Alton <mattalton97@gmail.com>
2023-12-29 16:17:58 +0000

    Added difficulty label to input

--- src/components/FoodList/FoodListDifficultyInput.tsx
+++ src/components/FoodList/FoodListDifficultyInput.tsx
@@ -3,14 +3,17 @@ import styles from "../../styles/slider.module.css";
 export default function FoodListDifficultyInput() {
   return (
     <div className="w-full">
-      <input
-        id="difficulty"
-        type="range"
-        min="1"
-        max="10"
-        defaultValue={6}
-        className={styles.slider}
-      />
+      <label className="block text-sm font-medium">
+        Difficulty
+        <input
+          id="difficulty"
+          type="range"
+          min="1"
+          max="10"
+          defaultValue={6}
+          className={styles.slider}
+        />
+      </label>
     </div>
   );
 }
