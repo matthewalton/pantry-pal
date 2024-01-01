ee1598eea55b308edb9b8bddddd3fdd678937c4c
Matt Alton <mattalton97@gmail.com>
2024-01-01 18:15:18 +0000

    Added home and profile header links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,28 @@
+import { getServerSession } from "next-auth/next";
 import LoginButton from "../Buttons/LoginButton";
+import { GET } from "@/app/api/auth/[...nextauth]/route";
+import Link from "next/link";
+import { Session } from "next-auth";
 
-export default function Header() {
+export default async function Header() {
+  const session = (await getServerSession(GET)) as Session | null;
+  console.log(session);
+
   return (
-    <div className="flex justify-end">
+    <div className="flex items-center gap-5 justify-end">
+      <Link href="/" className="transition-colors hover:text-green-200">
+        Home
+      </Link>
+
+      {session && (
+        <Link
+          href="/profile"
+          className="transition-colors hover:text-green-200"
+        >
+          Profile
+        </Link>
+      )}
+
       <LoginButton />
     </div>
   );
