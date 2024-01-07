aa4f2c7ba0c0bf221d909661b0cd455796d44bbd
Matt Alton <mattalton97@gmail.com>
2024-01-07 18:19:27 +0000

    Centered header text

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,7 @@ export default async function Header() {
   const session = (await getServerSession(GET)) as Session | null;
 
   return (
-    <header className="z-20 mx-auto flex h-14 w-full max-w-5xl flex-row flex-nowrap items-stretch justify-between py-3 duration-1000 ease-in-out animate-in fade-in slide-in-from-top-4">
+    <header className="z-20 mx-auto flex h-14 w-full max-w-5xl flex-row flex-nowrap items-center justify-between py-3 duration-1000 ease-in-out animate-in fade-in slide-in-from-top-4">
       <div className="font-bold">Pantry Pal</div>
 
       <div className="flex flex-wrap items-center gap-x-5 gap-y-1 justify-end">
