eb3219bbc94d4596ca86c92219e6bb2e2fc3039a
Matt Alton <mattalton97@gmail.com>
2024-01-08 20:23:55 +0000

    Fixed mobile header padding, always center body content

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,8 +8,8 @@ export default async function Header() {
   const session = (await getServerSession(GET)) as Session | null;
 
   return (
-    <header className="z-20 mx-auto flex h-14 w-full max-w-5xl flex-row flex-nowrap items-center justify-between py-3 duration-1000 ease-in-out animate-in fade-in slide-in-from-top-4">
-      <div className="font-bold">Pantry Pal</div>
+    <header className="z-20 mx-auto flex h-14 w-full max-w-5xl flex-row flex-nowrap items-center justify-between py-3 duration-1000 ease-in-out animate-in fade-in slide-in-from-top-4 px-4 sm:px-6">
+      <div className="font-bold whitespace-nowrap">Pantry Pal</div>
 
       <div className="flex flex-wrap items-center gap-x-5 gap-y-1 justify-end">
         <Link href="/" className="transition-colors hover:text-gray-700">
