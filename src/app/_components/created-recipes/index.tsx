6ef13cab2291c6a7ceafe725d9f97ccc55047a94
Matt Alton <mattalton97@gmail.com>
2024-01-16 21:59:11 +0000

    Altered appearance of created recipes

--- src/app/_components/created-recipes/index.tsx
+++ src/app/_components/created-recipes/index.tsx
@@ -11,10 +11,14 @@ export default async function CreatedRecipes() {
     <div>
       <h2 className="text-2xl font-medium mb-4">Recent Recipes</h2>
 
-      <div className="grid grid-cols-2 gap-3">
+      <div className="flex flex-wrap gap-x-4 gap-y-2">
         {recipes.map((recipe) => (
-          <div key={recipe.id} className="col">
-            <Link href={`/recipe/${recipe.uuid}`}>{recipe.title}</Link>
+          <div key={recipe.id}>
+            <Link href={`/recipe/${recipe.uuid}`}>
+              <div className="transition-colors px-4 py-2 rounded-xl border-2 border-dotted bg-white hover:bg-gray-100 text-gray-800 hover:text-gray-900">
+                {recipe.title}
+              </div>
+            </Link>
           </div>
         ))}
       </div>
